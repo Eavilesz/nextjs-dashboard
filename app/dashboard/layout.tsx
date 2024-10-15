54d6b81898bd57656dabd0d6083c2ef9231a2086
Eavilesz <ernesto-av@hotmail.com>
2024-10-15 09:00:24 -0500

    Removing ppr

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,8 +1,5 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
-// partial prerendering. This needs suspense on inner components. Also, don't forget to modify the next.config.json file with the ppr field.
-export const experimental_ppi = true;
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
